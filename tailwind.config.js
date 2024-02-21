cf86de0310b6dc6bd2871c994ffb21d57866a3fd
Krzysztof Dabrowski <10003141@ergo.com>
2024-02-21 12:48:11 +0100

    content

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
         'main': ['Poppins', 'sans-serif'],
         'heading': ['Noto Serif', 'sans-serif'],
       },
+      colors: {
+        'primary': {
+          '100': '#faf0e6',
+          '200': '#fff0db',
+          '300': '#eed9c4',
+          '400': '#e4d5b7',
+          '500': '#d9b99b',
+        },
+      },
     },
   },
   variants: {},
