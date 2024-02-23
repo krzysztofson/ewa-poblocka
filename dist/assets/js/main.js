bc05c7118658075d7f1465297205cf5af1838ad7
Krzysztof Dabrowski <10003141@ergo.com>
2024-02-23 14:05:10 +0100

    dyskografia

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,16 +1,13 @@
-const contactBtns = document.querySelectorAll('.js-contact-btn');
-const modal = document.querySelector('.js-modal');
-const modalBg = document.querySelector('.js-modal-bg');
-const modalClose = document.querySelector('.js-modal-close-btn');
+const navBtn = document.querySelector('.js-nav-btn');
+const nav = document.querySelector('.js-nav');
 
-function toggleModal() {
-    modal.classList.toggle('flex');
-    modal.classList.toggle('hidden');
+function togglenav() {
+    nav.classList.toggle('hidden');
+    nav.classList.toggle('flex');
 }
 
-contactBtns.forEach(btn => {
-    btn.addEventListener('click', toggleModal);
-})
+navBtn.addEventListener('click', togglenav);
 
-modalBg.addEventListener('click', toggleModal);
-modalClose.addEventListener('click', toggleModal);
+window.addEventListener('load', () => {
+    lightGallery(document.getElementById('lightgallery'));
+});
